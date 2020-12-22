f2540050ee3e9bc0d29804e304b1f9f71c87bd12
Gabrinawu <hl_2020@qq.com>
2020-12-22 20:10:31 +0800

    helloword eclipse

---
+++ java-practice/src/package1/HelloWorld.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello world");
+	}
+ 
+}
